b3850d4a99de777c3258c1bc7a3978bc75023c27
Flapper-Kor <Flapper.kor@icloud.com>
2024-02-09 22:30:57 +0900

    주석 내용 약간 수정

--- CH.-- Comment.playground/Contents.swift
+++ CH.-- Comment.playground/Contents.swift
@@ -82,3 +82,113 @@ func Hi(name : String? = "creep"){
 
 Hi(name: "slash")
 Hi()
+
+
+
+
+// MARK: - 작성 예)
+
+import SwiftUI
+extension Text {
+    /**
+     stroke and return the Text
+     This function is simply calling the shadow function as many times as the linewidth.
+     
+     > Usage:
+     >```
+     > var body : some View {
+     >    Text("hello").stroke(color: .black, lineWidth: 5)
+     > }
+     >```
+     
+     > Tip:
+     > this method return some View, not Text
+     > It should be called last.
+     > Use like below
+     >```
+     >  Text("Hello")
+     >      .font(.system(size: 48))
+     >      .foregroundColor(.white)
+     >      .stroke(color: .black, lineWidth: 4)
+     >```
+     
+     > Warning:
+     > Don't Use like below
+     >```
+     > Text("Hello")
+     >      .stroke(color: .black, lineWidth: 4)
+     >      .font(.system(size: 48))
+     >      .foregroundColor(.white)
+     >```
+     - Author: Flapper
+     - Parameters:
+        - color: Color for stroke
+        - lineWidth: Stroke/shadow width ( greater than 0 ).
+                     0 or a negative number returns itself as some View.
+                     this function just calling the shadow() function as many times as the linewidth.
+     
+     - returns: some View with stroke effect applied
+     */
+    func stroke(color: Color, lineWidth: Int) -> some View {
+        self.modifier(Stroked(color: color, lineWidth: lineWidth))
+    }
+    
+    struct Stroked : ViewModifier{
+        var color: Color
+        var lineWidth: Int
+        
+        func body(content: Content) -> some View {
+            applyShadow(content: AnyView(content), lineWidth: lineWidth)
+        }
+        
+        func applyShadow(content:AnyView, lineWidth: Int)-> AnyView{
+            if lineWidth <= 0{
+                return content
+            }else{
+                return applyShadow(content: AnyView(content.shadow(color: color, radius: 1)), lineWidth: lineWidth - 1)
+            }
+        }
+    }
+}
+
+// MARK: - template
+/**
+ stroke and return the Text
+ This function is simply calling the shadow function as many times as the linewidth.
+ 
+ > Usage:
+ >```
+ > var body : some View {
+ >    Text("hello").stroke(color: .black, lineWidth: 5)
+ > }
+ >```
+ 
+ > Tip:
+ > this method return some View, not Text
+ > It should be called last.
+ > Use like below
+ >```
+ >  Text("Hello")
+ >      .font(.system(size: 48))
+ >      .foregroundColor(.white)
+ >      .stroke(color: .black, lineWidth: 4)
+ >```
+ 
+ > Warning:
+ > Don't Use like below
+ >```
+ > Text("Hello")
+ >      .stroke(color: .black, lineWidth: 4)
+ >      .font(.system(size: 48))
+ >      .foregroundColor(.white)
+ >```
+ - Author: Flapper
+ - Parameters:
+    - color: Color for stroke
+    - lineWidth: Stroke/shadow width ( greater than 0 ).
+                 0 or a negative number returns itself as some View.
+                 this function just calling the shadow() function as many times as the linewidth.
+ 
+ - returns: some View with stroke effect applied
+ */
+
